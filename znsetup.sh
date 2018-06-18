ef21f9de65e5b73eae098cfdf2d96d6c930571cd
Keen Coder <aamarafie@github.com>
2018-06-18 20:41:29 +0300

    Znode Setup

--- znsetup.sh
+++ znsetup.sh
@@ -98,21 +98,22 @@ print_status "Updating system"
 sleep 5
 
 # update package and upgrade Ubuntu
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install git -y
 
 
 clear
 print_status "Installing dependencies"
 sleep 5
 
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev -y
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-sudo apt-get install libminiupnpc-dev libzmq3-dev
-sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev
+sudo apt-get install libminiupnpc-dev libzmq3-dev -y
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev -y
 
 clear
 print_status "Installing QT 5"
